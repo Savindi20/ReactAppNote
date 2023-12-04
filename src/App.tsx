cb168d07fdb533dac1c973bd6f2d4828e9e006b4
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-04 13:10:41 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      // <h1 className="text-3xl font-bold underline">
+      //   Hello world!
+      // </h1>
+      <>
+          <div style={{backgroundColor: "red", padding: 10, textAlign: "center"}}>Inline CSS BOX</div>
+          <br/>
+          <div className="box">External CSS BOX</div>
+          <br/>
+          <div className="bg-red p-10 text-align">Tailwind External utility BOX</div>
+      </>
   );
 }
 
