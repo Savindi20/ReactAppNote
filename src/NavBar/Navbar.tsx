5a98a0ff0e0965626759a8792f115d7931e5c490
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-11 22:27:54 +0530

    gradiant type changed

--- src/NavBar/Navbar.tsx
+++ src/NavBar/Navbar.tsx
@@ -12,8 +12,12 @@ export class Navbar extends Component {
 
                 {/*gradiant added ==> <div className="p-4 bg-blue-300 bg-gradient-to-r from-blue-900">*/}
                 {/*gradiant added ==> <div className="p-4 bg-blue-300 bg-gradient-to-r from-blue-90 to-red-500">*/}
+                {/*gradiant added ==> <div className="p-4 bg-blue-300 bg-gradient-to-br from-green-300 to-yellow-300">*/}
 
-                <div className="p-4 bg-blue-300 bg-gradient-to-r from-blue-90 to-red-500">
+                <div className="p-4 bg-blue-300
+                            bg-gradient-to-br
+                            from-green-300
+                            to-yellow-300">
                     <h1 className="text-4xl text-center text-tertiary">
                         This is Navbar!</h1>
                 </div>
