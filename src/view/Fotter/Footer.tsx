f6011d77b7581ec0bb4320b01d607e34a7dd2fbb
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-11 23:29:38 +0530

    organic shop footer added

--- src/view/Fotter/Footer.tsx
+++ src/view/Fotter/Footer.tsx
@@ -1,12 +1,16 @@
 import {Component} from "react";
+import logo from "../images/download.jpeg"
 
 export class Footer extends Component {
     render() {
         return (
             <>
-                <div className="p-4 bg-blue-300">
-                    <h1 className="text-4xl text-center text-tertiary">
-                        This is Footer!</h1>
+                <div className="p-2 bg-[#444544] flex justify-center">
+                    <p className="text[10px] text-[#e6f0e6] pr-2 hover:text-green-400">
+                        Copyright @ 2023</p>
+                    <h1 className="text-1xl text-secondary">
+                        Organic Shop</h1>
+                    <img className="h-5 w-5 top-0 ml-1" src={logo} alt=""/>
                 </div>
             </>
         );
