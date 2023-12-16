4a7da338a5aa29599c99b4bb3a12b7b498af5dc1
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-16 11:38:05 +0530

    login file updated

--- src/view/pages/Login/Login.tsx
+++ src/view/pages/Login/Login.tsx
@@ -3,9 +3,26 @@ import {Component} from "react";
 export class Login extends Component {
     render() {
         return (
-            <>
-                <h1 className="text-center text-3xl text-teal-700">This is Login</h1>
-            </>
+            <div className="flex h-screen bg-black">
+                <div className="h-auto mt-4 mb-4 pl-9 mx-auto border-gray-400 border-[0.5px] bg-white">
+                    <h2 className="text-3xl underline decoration-2 pt-2 pb-2">Sign In</h2>
+                    <form className="h-auto p-2 justify-center">
+                        <div className="pb-2">
+                            <label className="text-[20px]">Your Email</label>
+                            <input className="float-right border-[1px] border-black" type="email"/>
+                        </div>
+                        <div className="pb-2">
+                            <label className="text-[20px]">Password</label>
+                            <input className="float-right border-[1px] border-black" type="text"/>
+                        </div>
+                        <div className="mt-2">
+                            <button className="mt-5 p-5 bg-green-600 text-[20px] w-full" type="button">
+                                Sign In
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         );
     }
 }
