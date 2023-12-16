578258605e86b09f55140e82245721147498504c
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-16 13:48:31 +0530

    home page updated

--- src/view/pages/Home/Home.tsx
+++ src/view/pages/Home/Home.tsx
@@ -21,8 +21,11 @@ export class Home extends Component {
                                                text-[12px] pl-2">
                                     Spinach</h3>
                             </div>
+                            <div className={"bg-yellow-300 ml-1 p-[0.3px] rounded-lg pr-2"}>
+                                <h3 className="text-[12px]">200 <small className={"text-[10px]"}>LKR</small> </h3>
+                            </div>
                             <div>
-                                <h3 className="text-[12px]">200 LKR</h3>
+
                             </div>
                         </div>
                     </div>
