e4caeb35cf6a5cf74479d7d24fca25f063426b25
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-23 14:00:09 +0530

    shopping cart page basic content added

--- src/view/pages/ShoppingCart/ShoppingCart.tsx
+++ src/view/pages/ShoppingCart/ShoppingCart.tsx
@@ -1,12 +1,27 @@
 import {Component} from "react";
+import { CartItem } from "../../../model/CartItem";
 
-export class ShoppingCart extends Component {
+interface ShoppingCartProps{
+    itemList: CartItem[];
+}
+
+export class ShoppingCart extends Component<ShoppingCartProps> {
 
     render() {
 
         return (
            <>
-           <h1>Shopping Cart</h1>
+          <div className="flex justify-center items-center min-h-screen">
+            <table className="w-full m-6">
+                <tr className="bg-gray-400">
+                    <th className="text-[9px] font-normal border-black border-[0.5px] px-1">Id</th>
+                    <th className="text-[9px] font-normal border-black border-[0.5px] px-1">Name</th>
+                    <th className="text-[9px] font-normal border-black border-[0.5px] px-1">UnitPrice</th>
+                    <th className="text-[9px] font-normal border-black border-[0.5px] px-1">Quantity</th>
+                    <th className="text-[9px] font-normal border-black border-[0.5px] px-1">TotalPrice</th>
+                </tr>
+            </table>
+          </div>
            </>
         );
     }
