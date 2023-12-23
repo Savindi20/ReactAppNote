0de5cef1561735d4a3b96d74d5a5cfc520d8fd54
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-23 14:13:28 +0530

    shopping cart updated

--- src/view/pages/ShoppingCart/ShoppingCart.tsx
+++ src/view/pages/ShoppingCart/ShoppingCart.tsx
@@ -20,6 +20,21 @@ export class ShoppingCart extends Component<ShoppingCartProps> {
                     <th className="text-[9px] font-normal border-black border-[0.5px] px-1">Quantity</th>
                     <th className="text-[9px] font-normal border-black border-[0.5px] px-1">TotalPrice</th>
                 </tr>
+                {
+                    this.props.itemList.length === 0 ?
+                        <tr>
+                            <td colSpan={5}
+                                    className="border-black
+                                    border-[0.5px] px-1">
+                                <p className="text-center
+                                    text-[8px]">No Items to Display!
+                                </p>
+                            </td>
+                        </tr>
+                    : this.props.itemList.map((item) => (
+                        <tr className="border-black border-[0.5px] px-1"></tr>
+                    ))
+                }
             </table>
           </div>
            </>
