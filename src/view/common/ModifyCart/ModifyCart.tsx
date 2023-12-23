a64db7813ba9cf68c6e33d00977df014b21efce8
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-23 11:03:57 +0530

    increase decrease item count added

--- src/view/common/ModifyCart/ModifyCart.tsx
+++ src/view/common/ModifyCart/ModifyCart.tsx
@@ -19,12 +19,22 @@ export class ModifyCart extends Component<ModifyCartProps, ModifyCartState> {
 
     render() {
         const itemCount = this.state.itemCount;
-        
+
+        const onIncreaseItemCount = () => {
+            this.setState({itemCount:itemCount+1})
+        }
+
+        const onDecreaseItemCount = () => {
+            if(itemCount>0){
+                this.setState({itemCount:itemCount-1})
+            }
+        }
+
         return (
             <div className="w-full mt-1 p-[2.4px] bg-secondary text-[8px] text-center border-gray-500 border-[0.5px] bg-red-400">
-                <button className="float-left text-[8px] bg-yellow-400 rounded-lg h-3 w-4">-</button>
+                <button className="float-left text-[8px] bg-yellow-400 rounded-lg h-3 w-4" onClick={onDecreaseItemCount}>-</button>
                 <small className=" text-[8px]">{itemCount}</small>
-                <button className="float-right text-[8px] bg-yellow-400 rounded-lg h-3 w-4">+</button>
+                <button className="float-right text-[8px] bg-yellow-400 rounded-lg h-3 w-4" onClick={onIncreaseItemCount}>+</button>
             </div> 
         )
     
