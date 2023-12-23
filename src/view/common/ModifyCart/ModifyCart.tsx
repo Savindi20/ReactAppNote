d2c0412263e9f902c21d5c60055328e0902dab21
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-23 10:58:41 +0530

    item count const added

--- src/view/common/ModifyCart/ModifyCart.tsx
+++ src/view/common/ModifyCart/ModifyCart.tsx
@@ -18,10 +18,12 @@ export class ModifyCart extends Component<ModifyCartProps, ModifyCartState> {
     }
 
     render() {
+        const itemCount = this.state.itemCount;
+        
         return (
             <div className="w-full mt-1 p-[2.4px] bg-secondary text-[8px] text-center border-gray-500 border-[0.5px] bg-red-400">
                 <button className="float-left text-[8px] bg-yellow-400 rounded-lg h-3 w-4">-</button>
-                <small className=" text-[8px]">1</small>
+                <small className=" text-[8px]">{itemCount}</small>
                 <button className="float-right text-[8px] bg-yellow-400 rounded-lg h-3 w-4">+</button>
             </div> 
         )
